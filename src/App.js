48cfb8f4a6a2e7023143720724ade9ed33d140bf
Neilphpbeginner <lemmer.neil@gmail.com>
2022-08-16 21:45:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome name="Joe Soap" age="39" />
+      <Welcome name="John Handcock" age="52" />
     </div>
   );
 }
