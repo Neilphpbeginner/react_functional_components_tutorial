514233497491863ce1c1fd704c399e5bb4dab382
Neilphpbeginner <lemmer.neil@gmail.com>
2022-08-17 21:26:51 +0200

    Adding more content to my tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import Welcome from "./components/Welcome";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Joe Soap" age="39" />
-      <Welcome name="John Handcock" age="52" />
+      <Counter />
     </div>
   );
 }
